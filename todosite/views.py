be03e908b93fc8d2af5d3e5c61ee1e7d4e4e30d7
svekla <egor.koven757@gmail.com>
2026-05-14 12:53:12 +0300

    feat: add notes sorting by date

--- todosite/views.py
+++ todosite/views.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 
 def index(request):
-    notes = Note.objects.all().order_by("completed", "deadline")
+    notes = Note.objects.all().order_by("-deadline")
     context = {"notes": notes, "today": timezone.now().date()}
 
     return render(request, "todosite/index.html", context=context)
